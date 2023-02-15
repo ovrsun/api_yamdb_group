925c18908367659d84dc3010eef36824cc0aa291
Леонид Ермошин <rok_52@mail.ru>
2023-02-15 04:22:28 +0300

    сделал модельки

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Review(models.Model):
+    """Модель отзывов к произведениям"""
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Произведения',
+        )
+    text = models.TextField(max_length=300)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='автор',
+        )
+    score = models.IntegerField(
+        verbose_name='Оценка отзыва',
+        validators=(
+            MinValueValidator(1),
+            MaxValueValidator(10),
+            )
+        )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        db_index=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[20]
+
+
+class Comment(models.Model):
+    """Модель для коменнтариев"""
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв',
+    )
+    text = models.CharField(
+        max_length=200,
+        verbose_name='Тест коментария',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='автор комента',
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True,
+        db_index=True,
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
