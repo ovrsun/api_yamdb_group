fa1dd3fd9ce3095fde835e380dac91acbdcb163e
Alexey Targanov <siddhett@gmail.com>
2023-02-26 16:42:26 +0100

    Add bulk create

--- api_yamdb/loadcsv/management/commands/load_csv.py
+++ api_yamdb/loadcsv/management/commands/load_csv.py
@@ -1,109 +1,129 @@
 import csv
+import os
+import sqlite3
+
+from django.conf import settings
 from django.core.management import BaseCommand
+
 from reviews.models import Category, Comment, Genre, Title, Review, Genre_Title
 from users.models import CustomUser
+
+
+DB_PATH = os.path.join(settings.BASE_DIR, 'db.sqlite3')
 
 
 class Command(BaseCommand):
     help = 'description'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--clear',
+            action='store_true',
+            help='Clear all tables in data base.'
+        )
+
+    def clear_db(self):
+        con = sqlite3.connect(DB_PATH)
+        cur = con.cursor()
+
+        cur.executescript('''
+        UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'reviews_category';
+        UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'reviews_title';
+        UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'reviews_genre';
+        UPDATE `sqlite_sequence` SET `seq` = 99 WHERE `name` = 'users_customuser';
+        UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'reviews_review';
+        UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'reviews_genre_title';
+        UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'reviews_comment';
+        ''')
+        con.commit()
+        con.close()
+
     def categories_load(self):
         file_path = 'static/data/category.csv'
         with open(file_path, "r") as csv_file:
-            data = list(csv.reader(csv_file, delimiter=","))
-            for row in data[1:]:
-                Category.objects.create(name=row[1], slug=row[2])  # Давайте перепишем на bulk_create, иначе будем грустно делать кучу запросов, если файлы будут большими
+            data = list(csv.DictReader(csv_file, delimiter=","))
+            Category.objects.bulk_create([Category(**row) for row in data])
 
     def genres_load(self):
         file_path = 'static/data/genre.csv'
         with open(file_path, "r") as csv_file:
             data = list(csv.DictReader(csv_file, delimiter=","))
-            for row in data:
-                Genre.objects.create(name=row['name'], slug=row['slug'])
+            Genre.objects.bulk_create([Genre(**row) for row in data])
 
     def users_load(self):
         file_path = 'static/data/users.csv'
         with open(file_path, "r") as csv_file:
             data = list(csv.DictReader(csv_file, delimiter=","))
-            for row in data:
-                CustomUser.objects.create(
-                    username=row['username'],
-                    email=row['email'],
-                    role=row['role'],
-                    bio=row['bio'],
-                    first_name=row['first_name'],
-                    last_name=row['last_name'],
-                )
+            CustomUser.objects.bulk_create([CustomUser(**row) for row in data])
 
     def titles_load(self):
         file_path = 'static/data/titles.csv'
         with open(file_path, "r") as csv_file:
             data = list(csv.DictReader(csv_file, delimiter=","))
-            for row in data:
-                try:
-                    category, _ = Category.objects.get_or_create(
-                        pk=row['category']
-                    )
-                    num = row['category']
-                    category = Category.objects.get(pk=num)
-                    Title.objects.create(
-                        name=row['name'],
-                        year=row['year'],
-                        category_id=category.id
-                    )
-                except Category.DoesNotExist as e:
-                    print(e)
+            Title.objects.bulk_create([Title(
+                id=row['id'],
+                name=row['name'],
+                year=row['year'],
+                category=Category.objects.get(pk=row['category'])
+            ) for row in data])
 
     def reviews_load(self):
         file_path = 'static/data/review.csv'
         with open(file_path, "r") as csv_file:
             data = list(csv.DictReader(csv_file, delimiter=","))
-            for row in data:
-                title, _ = Title.objects.get_or_create(pk=row['title_id'])
-                author = CustomUser.objects.get(pk=row['author'])
-                Review.objects.create(
-                    title=title,
+            Review.objects.bulk_create([
+                Review(
+                    id=row['id'],
+                    title=Title.objects.get(pk=row['title_id']),
                     text=row['text'],
-                    author_id=author.id,
+                    author=CustomUser.objects.get(pk=row['author']),
                     score=row['score'],
                     pub_date=row['pub_date']
                 )
+                for row in data
+            ])
 
     def comments_load(self):
         file_path = 'static/data/comments.csv'
         with open(file_path, "r") as csv_file:
             data = list(csv.DictReader(csv_file, delimiter=","))
-            for row in data:
-                review, _ = Review.objects.get_or_create(pk=row['review_id'])
-                author = CustomUser.objects.get(pk=row['author'])
-                Comment.objects.create(
-                    review_id=review.id,
+            Comment.objects.bulk_create([
+                Comment(
+                    id=row['id'],
+                    review=Review.objects.get(pk=row['review_id']),
                     text=row['text'],
-                    author_id=author.id,
+                    author=CustomUser.objects.get(pk=row['author']),
                     pub_date=row['pub_date']
                 )
+                for row in data
+            ])
 
     def genre_title_load(self):
         file_path = 'static/data/genre_title.csv'
         with open(file_path, "r") as csv_file:
             data = list(csv.DictReader(csv_file, delimiter=","))
-            for row in data:
-                title, _ = Title.objects.get_or_create(pk=row['title_id'])
-                genre = Genre.objects.get(pk=row['genre_id'])
-                Genre_Title.objects.create(
-                    title_id=title.id,
-                    genre_id=genre.id,
+            Genre_Title.objects.bulk_create([
+                Genre_Title(
+                    id=row['id'],
+                    title=Title.objects.get(pk=row['title_id']),
+                    genre=Genre.objects.get(pk=row['genre_id'])
                 )
+                for row in data
+            ])
 
     def handle(self, *args, **options):
-        Category.objects.all().delete()  # Удалять старые записи стоит только если мы явно хотим это сделать, передав какой-то специальный параметр в команду.
-        Title.objects.all().delete()
-        Genre.objects.all().delete()
-        Review.objects.all().delete()
+        if options['clear']:
+            Category.objects.all().delete()
+            Title.objects.all().delete()
+            Genre.objects.all().delete()
+            Review.objects.all().delete()
+            CustomUser.objects.exclude(pk=1).delete()
+            self.clear_db()
+
         self.categories_load()
         self.genres_load()
         self.users_load()
         self.titles_load()
         self.reviews_load()
         self.comments_load()
-        self.genre_title_load()  # Надо переписать на bulk_create все, что можно. Оптимизируем загрузку
+        self.genre_title_load()
